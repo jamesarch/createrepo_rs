74e2912296b84e1932e215e9adda79d85c292b02
jamesarch <han.shan@live.cn>
2026-04-28 11:56:59 +0800

    fix: resolve CI failures (rustfmt + clippy for Rust 1.95)

--- compression/zstd.rs
+++ compression/zstd.rs
@@ -1,9 +1,8 @@
 pub fn decompress(data: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-    zstd::decode_all(data).map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
+    zstd::decode_all(data).map_err(std::io::Error::other)
 }
 
 pub fn compress(data: &[u8], level: i32) -> Result<Vec<u8>, std::io::Error> {
-    let encoded = zstd::encode_all(data, level)
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+    let encoded = zstd::encode_all(data, level).map_err(std::io::Error::other)?;
     Ok(encoded)
 }
